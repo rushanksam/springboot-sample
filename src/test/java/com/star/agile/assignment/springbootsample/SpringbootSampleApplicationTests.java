b891713c347556de2a83b5143e498a89ec50ad62
Rushank <sambrekarrushank@gmail.com>
2024-03-22 23:37:13 +0530

    first commit

---
+++ src/test/java/com/star/agile/assignment/springbootsample/SpringbootSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.star.agile.assignment.springbootsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
